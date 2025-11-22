4614da2ef712deff1038472ad63abc3f4f4c1928
vilohithreddy <vilohithreddysheri@gmail.com>
2025-11-22 21:47:05 +0530

    print

---
+++ print.py
@@ -0,0 +1,11 @@
+ # printing a greeting message
+print("Hello , World!")
+
+# printing my name
+print("My name is Alex.")
+
+# printing a fun fact about me
+print("I love playing cricket")
+
+# performing and printing a simple calculation
+print("the result of 3 + 5 is:",3 + 5)
