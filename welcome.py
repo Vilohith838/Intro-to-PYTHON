4e1c2ae46278a52cb1997db5b7234862b61cd71a
Vilohith838 <vilohithreddysheri@gmail.com>
2025-11-23 14:53:21 +0530

    Add files via upload

---
+++ welcome.py
@@ -0,0 +1,14 @@
+# print a sentence
+print("!Welcome to Codingal !")
+
+# print a number
+print("4")
+
+# next line after print
+print("Hello \n Python")
+
+# pass multiple arguments for printing
+print("hello , 5")
+
+#end argument of print statement
+print("Welcome to " , end="*")
