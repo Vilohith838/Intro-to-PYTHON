5c02bc6fd237e6bf2e9cdd94bf698b314540c2da
Vilohith838 <vilohithreddysheri@gmail.com>
2025-11-27 22:41:52 +0530

    Add files via upload

---
+++ variables acp.py
@@ -0,0 +1,14 @@
+# Storing birthday dates in 5 different variables
+
+birthday1 = "12 March 2010"
+birthday2 = "25 July 2012"
+birthday3 = "03 November 2008"
+birthday4 = "19 April 2011"
+birthday5 = "07 January 2013"
+
+# Printing the stored birthday dates
+print("Birthday 1:", birthday1)
+print("Birthday 2:", birthday2)
+print("Birthday 3:", birthday3)
+print("Birthday 4:", birthday4)
+print("Birthday 5:", birthday5)
