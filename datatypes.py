7bfe9e1b62bcdf35acf1a9ea5c861251a6207868
Vilohith838 <vilohithreddysheri@gmail.com>
2025-11-29 21:27:39 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,16 @@
+x = 10
+a = 5
+print("type of a:", type(a))
+
+
+b = 2.5
+print("type of b:", type(b))
+
+c = "vilohith"
+print("type of c:", type(c))
+
+d = False
+print("type of d:", type(d))
+
+f = True
+print("type :", type(f))
