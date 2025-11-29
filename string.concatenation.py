122a594befe16de80726e0d6057176996daa567d
Vilohith838 <vilohithreddysheri@gmail.com>
2025-11-29 21:35:16 +0530

    Add files via upload

---
+++ string.concatenation.py
@@ -0,0 +1,11 @@
+print("Concatenate the strings")
+print("Enter the first string:")
+strOne = input()
+
+print("Enter the second string:")
+strTwo = input()
+
+#Concatenate the two strings
+#using addition operator
+strThree =  strOne + strTwo
+print("\nConcatenated the string: ", strThree)
