972dcb94471f3a0053bb92308b375ba04de978cf
Vilohith838 <vilohithreddysheri@gmail.com>
2025-12-04 21:41:57 +0530

    Add files via upload

---
+++ square root acp.py
@@ -0,0 +1,10 @@
+import math
+
+# ask the user for a number
+num = float(input("Enter a number to find its square root: "))
+
+# calculate square root
+square_root = math.sqrt(num)
+
+# display result
+print("The square root of", num, "is:", square_root)
