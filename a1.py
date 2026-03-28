7b5c0b9df70164037e444a3c9e8d2889a2c9e9af
Vilohith838 <vilohithreddysheri@gmail.com>
2026-03-28 21:15:10 +0530

    Add files via upload

--- a1.py
+++ a1.py
@@ -1,17 +1,8 @@
-import pygame
+from tkinter import *
 
-pygame.init()
+window = Tk()
 
+window.title('Demo Window')
+window.geometry('400x300')
 
-screen = pygame.display.set_mode((400,500))
-
-done = False
-
-while not done:
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-
-
-    pygame.display.flip()
+window.mainloop()
