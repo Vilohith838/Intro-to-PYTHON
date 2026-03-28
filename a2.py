7529af68f755266204b456f34e176a873359e54c
Vilohith838 <vilohithreddysheri@gmail.com>
2026-03-28 21:42:23 +0530

    Add files via upload

--- a2.py
+++ a2.py
@@ -1,20 +1,38 @@
-import pygame
-pygame.init()
-window = pygame.display.set_mode((400, 400))
-window.fill((255, 255, 255))
-GREEN = (0, 255, 0)
+from tkinter import *
+from datetime import date
 
-pygame.draw.circle(window, GREEN, (300, 300), 50)
+root = Tk()
+root.title('getting started with widgets')
+root.geometry('400x300')
 
-pygame.draw.circle(window, GREEN, (100, 100), 50, 3)
 
-pygame.display.update()
+lb1 = Label(text="Hey There!", fg ="white", bg = "#072F5F",
+height = 1, width = 300)
 
-running = True
+name_lb1 = Label(text="Full Nme!", bg = "#3895D3")
+name_entry = Entry()
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+def display():
 
-pygame.quit()
+    name = name_entry.get()
+
+    global Message
+    message = "Welcome to the Apllication! \n Today's date is: "
+    greet = "Home "+name+"\n"
+
+    text_box.insert(END, greet)
+    text_box.insert(END, message)
+    text_box.insert(END, date.today())
+
+text_box = Text(height=3)   
+
+btn = Button(text="Begin", command=display, height=1, 
+bg = "#1261A0" , fg="white")
+
+lb1.pack()
+name_lb1.pack()
+name_entry.pack()
+btn.pack()
+text_box.pack()
+
+root.mainloop()
