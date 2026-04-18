c93c9011ff7fdf067ee955f8d1bbe4bf8b9c5b47
Vilohith838 <vilohithreddysheri@gmail.com>
2026-04-18 21:31:25 +0530

    Add files via upload

--- a1.py
+++ a1.py
@@ -1,8 +1,10 @@
-from tkinter import *
+import numpy as np
+data_type = [('name', 'S15'), ('class', 'int'), ('height', 'float')]
+students_details = [('James', 5, 48.5), ('Nail', 6, 52.5),
+('paul', 5, 42.10), ('pit', 5, 42.11)]
 
-window = Tk()
-
-window.title('Demo Window')
-window.geometry('400x300')
-
-window.mainloop()
+students = np.array(students_details, dtype = data_type)
+print("Original array:")
+print(students)
+print("sort by height")
+print(np.sort(students, order='height'))
